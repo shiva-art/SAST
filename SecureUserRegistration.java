3e5653aaad057c4b75ba8e55dd093703a26468ee
shiva-art <71481826+shiva-art@users.noreply.github.com>
2023-08-21 15:23:14 +0530

    Add files via upload

---
+++ SecureUserRegistration.java
@@ -0,0 +1,82 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SecureUserRegistration {
+
+    private static Map<String, String> userDatabase = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Select an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    registerUser(scanner);
+                    break;
+                case 2:
+                    loginUser(scanner);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+
+    private static void registerUser(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        String hashedPassword = hashPassword(password);
+        userDatabase.put(username, hashedPassword);
+        System.out.println("User registered successfully.");
+    }
+
+    private static void loginUser(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        String hashedPassword = hashPassword(password);
+        String storedPassword = userDatabase.get(username);
+
+        String name = request.getParameter("username");
+        String pwd = requyest.getParameter("password");
+        Statement statement = connection.createStatement(); 
+        String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
+        String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
+        ResultSet result = statement.executeQuery(query);
+    }
+
+    private static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(password.getBytes());
+            StringBuilder hashString = new StringBuilder();
+
+            for (byte hashByte : hashBytes) {
+                hashString.append(Integer.toHexString(0xFF & hashByte));
+            }
+
+            return hashString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hashing algorithm not available", e);
+        }
+    }
+}
